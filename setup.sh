f557f99092959bf2d3032560f2c00d9d75f2b5b9
Babu Natarajan <babunatarajan6@gmail.com>
2021-01-16 15:32:33 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -2,12 +2,13 @@
 
 service nagios-nrpe-server stop
 apt -y remove nagios-nrpe-server
-apt -y remove nagios-nrpe-server
-cd /tmp
+apt-get -y install build-essential libssl-dev
+
+cd /tmp/nrpe_agent_source_install
 wget --no-check-certificate -O nrpe.tar.gz https://github.com/NagiosEnterprises/nrpe/archive/nrpe-4.0.3.tar.gz
 tar xzf nrpe.tar.gz
 
-cd /tmp/nrpe-nrpe-4.0.3/
+cd /tmp/nrpe_agent_source_install/nrpe-nrpe-4.0.3/
 ./configure --enable-command-args
 make all
 make install-groups-users
@@ -15,9 +16,7 @@ make install
 make install-config
 make install-init
 update-rc.d nrpe defaults
-cd /tmp/nrpe_agent_source_install
 cp /tmp/nrpe_agent_source_install/nrpe.cfg /usr/local/nagios/etc/nrpe.cfg
 cp /tmp/nrpe_agent_source_install/check_memory /usr/local/nagios/libexec/check_memory
-service nagios-nrpe-server start
+service nrpe start
 netstat -tupln | grep 5666
-
