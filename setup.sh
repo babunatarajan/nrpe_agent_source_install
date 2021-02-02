8d4e26e1def05c1ca9b8901943e2f81f8401351d
Babu Natarajan <babunatarajan6@gmail.com>
2021-02-02 15:08:11 -0500

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -22,7 +22,9 @@ make install-init
 update-rc.d nrpe defaults
 cp /tmp/nrpe_agent_source_install/nrpe.cfg /usr/local/nagios/etc/nrpe.cfg
 cp /tmp/nrpe_agent_source_install/check_memory /usr/local/nagios/libexec/check_mem
-service nrpe start
+systemctl daemon-reload
+service nrpe restart
+service nrpe status
 netstat -tupln | grep 5666
 
 #Install the Nagios Plugins
